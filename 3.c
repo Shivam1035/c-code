082a15e2531bfa8ed61e6e3da27f3fef6ccd8a26
Shivam Tiwari <110774112+Shivam1035@users.noreply.github.com>
2022-08-21 03:15:16 +0530

    Add files via upload

--- 3.c
+++ 3.c
@@ -1,21 +1,20 @@
-//Write a program in c to check a given number is even or odd using unction.
-#include<stdio.h>
-	int sk(int a)
-	{
-		if((a % 2)!=0)
-		{
-			printf("This is odd number:");
-		}
-		else
-		{
-			printf("This is even number:");
-		}
-	}
-	int main()
-	{
-		int x;
-		printf("Enter your number:");
-		scanf("%d",&x);
-		sk(x);
-		return 0;
-	}
+// 3. Write a program in C to copy the elements of one array into another array.
+#include <stdio.h>
+int main()
+{
+    int arr1[4], arr2[4];
+    for (int i = 0; i < 4; i++)
+    {
+        printf("Element %d : - ", i);
+        scanf("%d", &arr1[i]);
+    }
+    for (int j = 0; j < 4; j++)
+    {
+        arr2[j] = arr1[j];
+    }
+    for (int k = 0; k < 4; k++)
+    {
+        printf("%d ", arr2[k]);
+    }
+    return 0;
+}
